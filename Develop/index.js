a858fc8198adbff6fb356865dade663cbce47e97
Fancy Nancy <ramireznancy.686@gmail.com>
2023-08-22 13:38:42 -0600

    added fetchlicensename function and integrated it to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,54 +1,77 @@
 // Included packages needed for this application
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const {
+  generateMarkdown,
+  fetchLicenseNames,
+  fetchLicense,
+} = require("./utils/generateMarkdown");
 const fs = require("fs");
+//fetch the licenses from github
+fetchLicenseNames()
+  .then((licenses) => {
+    const licenseChoices = licenses.concat(["None"]);
+    const questions = [
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Project Description",
+      },
+      {
+        type: "input",
+        name: "installation steps",
+        message: "What are the steps required to install your project?",
+      },
+      {
+        type: "input",
+        name: "Usage",
+        message: "How do you use you project?",
+      },
+      {
+        type: "input",
+        name: "Credits",
+        message:
+          "Did you have any collaborators, third party assets or any tutorials you followed?",
+      },
+      {
+        type: "list",
+        name: "License",
+        message: "What license did you use?",
+        choices: licenseChoices,
+      },
+    ];
 
-//Created an array of questions for user input
-const questions = [
-  {
-    question: "What is the name of your project?",
-    heading: "Title",
-    input: "title",
-  },
-  {
-    question: "Describe you project",
-    heading: "Project Description",
-    input: "projectDescription",
-  },
-  {
-    question: "What are the steps required to install your project?",
-    heading: "Installation",
-    input: "installationSteps",
-  },
-  {
-    question: "How do you use your project?",
-    heading: "Usage",
-    input: "usage",
-  },
-  {
-    question:
-      "Did you have any collaborators, third party assets or any tutorials you followed?",
-    heading: "Credits",
-    input: "credits",
-  },
-];
-
-//Created a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (error) => {
-    if (err) {
-      console.log(error);
-    } else {
-      console.log("README file had been created");
+    //Created a function to write README file
+    function writeToFile(fileName, data) {
+      fs.writeFile(fileName, data, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("README file had been created");
+        }
+      });
     }
+    // Created a function to initialize app
+    function init() {
+      inquirer.prompt(questions).then((answers) => {
+        fetchLicense(answers.license)
+          .then((licenseData) => {
+            answers.licenseData = licenseData;
+            const markdown = generateMarkdown(answers);
+            writeToFile("README.md", markdown);
+          })
+          .catch((error) => {
+            console.error("Error fetching license data:", error);
+          });
+      });
+    }
+    //Function call to initialize app
+    init();
+  })
+  .catch((error) => {
+    console.error("Error fetching license names:", error);
   });
-}
-// Created a function to initialize app
-function init() {
-  inquirer.prompt(questions).then((answers) => {
-    const markdown = generateMarkdown(answers);
-    writeToFile("README.md, markdown");
-  });
-}
-//Function call to initialize app
-init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,75 @@
+function fetchLicense(licenseName) {
+  const apiKey = "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";
+  const url = `https://api.github.com/licenses/${licenseName}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const licenseData = {
+        name: data.name,
+        badge: data.spdx_id,
+        link: data.html_url,
+      };
+      return licenseData;
+    })
+    .catch((error) => {
+      console.error("Error fetching license data:", error);
+      return "";
+    });
+}
+
+function fetchLicenseNames() {
+  const url = "https://api.github.com/licenses";
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const licenseNames = data.map((license) => license.key);
+      return licenseNames;
+    })
+    .catch((error) => {
+      console.error("Error fetching license names:", error);
+      return [];
+    });
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenseData) {
+  if (licenseData && licenseData.badge) {
+    return `![License](https://img.shields.io/badge/License-${licenseData.badge}-blue.svg)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseData) {
+  if (licenseData && licenseData.link) {
+    return licenseData.link;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseData) {
+  if (licenseData && licenseData.name) {
+    return `## License\n\nThis project is licensed under the [${licenseData.name}](${licenseData.link}) license.`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licensePromise = fetchLicenseData(data.license);
+  return licensePromise.then((licenseData) => {
+    const licenseBadge = renderLicenseBadge(licenseData);
+    const licenseLink = renderLicenseLink(licenseData);
+    const licenseSection = renderLicenseSection(licenseData);
+
+    return `# ${data.title}
   ${licenseBadge}
 
   ## Description
@@ -20,7 +77,7 @@ function generateMarkdown(data) {
   ${data.projectDescription}
 
   ## Table of Contents
-  - [Installation](#installation
+  - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
   
@@ -39,6 +96,7 @@ function generateMarkdown(data) {
   ${data.credits}
   
   ${licenseSection}`;
+  });
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown, fetchLicenseNames, fetchLicense };
